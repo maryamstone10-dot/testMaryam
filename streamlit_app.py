5ef9c9f0c2930d9f86891ebbba13c06a35da1e33
maryamstone10-dot <maryamstone10@gmail.com>
2025-10-01 16:12:52 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import numpy as np
-from keras.models import load_model
+
 
 from keras.models import load_model  # TensorFlow is required for Keras to work
 from PIL import Image, ImageOps  # Install pillow instead of PIL
